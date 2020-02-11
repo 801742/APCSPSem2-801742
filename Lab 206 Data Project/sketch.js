2ab3fc2aa6913c9246a723874e42fd5f91e924d6
801742 <54591166+801742@users.noreply.github.com>
2020-02-11 08:49:45 -0800

    jj

---
+++ Lab 206 Data Project/sketch.js
@@ -0,0 +1,27 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+var player;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+ player = 'Kobe Bryant'
+}
+
+//  The draw function is called @ 30 fps
+function draw(){
+}
+
+function loadPlayerStats(player) {
+// column 2 has the player's name in the stats table
+statsArray = totals_stats.findRows(player, 2); if (statsArray.length === 0) {
+
+
+
+function getStat(){
+  if(player = 'kobe')
+}
